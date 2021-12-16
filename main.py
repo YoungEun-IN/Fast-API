3c8057183dee911dea0baff2af431da5cd88ac9d
Youngeun IN <yyoungeunin@gmail.com>
2021-12-17 01:24:56 +0900

    웹 CRUD MySQL 연동

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-# https://github.com/hogeline/sample_fastapi
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-from fastapi import FastAPI
-from typing import List
 from starlette.middleware.cors import CORSMiddleware
 
 from db import session
 from model import UserTable, User
+
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
@@ -18,47 +20,83 @@ app.add_middleware(
 )
 
 
+@app.get("/")
+async def root():
+    return {"message": "/users 에서 사용자관리"}
+
+
 # ----------API 정의------------
-@app.get("/users")
-def read_users():
+@app.get("/users", response_class=HTMLResponse)
+async def read_users(request: Request):
+    print("read_users >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    context = {}
+
     users = session.query(UserTable).all()
-    return users
+
+    context['request'] = request
+    context['users'] = users
+
+    return templates.TemplateResponse("user_list.html", context)
 
 
-@app.get("/users/{user_id}")
-def read_user(user_id: int):
+@app.get("/users/{user_id}", response_class=HTMLResponse)
+async def read_user(request: Request, user_id: int):
+    print("read_user >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    context = {}
+
     user = session.query(UserTable).filter(UserTable.id == user_id).first()
-    return user
+    print(user.name)
+    context['name'] = user.name
+    context['age'] = user.age
+    context['request'] = request
+
+    return templates.TemplateResponse("user_detail.html", context)
 
 
-@app.post("/user")
-# /user?name="이름"&age=10
-async def create_user(name: str, age: int):
+@app.post("/users")
+async def create_user(users: User):
+    print("create_user >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    # data = await request.json()
+
+    userlist = list(users)
+    uname = userlist[1][1]
+    uage = userlist[2][1]
+
     user = UserTable()
-    user.name = name
-    user.age = age
+    user.name = uname
+    user.age = uage
 
     session.add(user)
     session.commit()
 
-    return f"{name} created..."
+    return {'result_msg': f'{uname} Registered...'}
 
 
 @app.put("/users")
-# users=[{"id": 1, "name": "이름1", "age": 16},{"id": 2, "name": "이름2", "age": 20}]
-async def update_users(users: List[User]):
-    for i in users:
-        user = session.query(UserTable).filter(UserTable.id == i.id).first()
-        user.name = i.name
-        user.age = i.age
-        session.commit()
+async def modify_users(users: User):
+    print("modify_user >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
-    return f"{users[0].name} updated..."
+    userlist = list(users)
+    uid = userlist[0][1]
+    uname = userlist[1][1]
+    uage = userlist[2][1]
 
-
-@app.delete("/user")
-async def delete_users(userid: int):
-    user = session.query(UserTable).filter(UserTable.id == userid).delete()
+    user = session.query(UserTable).filter(UserTable.id == uid).first()
+    user.name = uname
+    user.age = uage
     session.commit()
 
-    return f"User deleted..."
+    return {'result_msg': f"{uname} updated..."}
+
+
+@app.delete("/users")
+async def delete_users(users: User):
+    print("delete_user >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+    userlist = list(users)
+    uid = userlist[0][1]
+
+    user = session.query(UserTable).filter(UserTable.id == uid).delete()
+    session.commit()
+
+    return {'result_msg': f"User deleted..."}
